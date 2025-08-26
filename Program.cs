7df4966e585f896ead796b9c98adde538f1121d2
Omar Horacio <odelgadillo@outlook.com>
2025-08-25 21:10:33 -0400

    Agregar conexion a la base de datos

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
+using BibliotecaAPI.Datos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //area de srvicios
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
+    opciones.UseSqlServer("name=DefaultConnection"));
 
 var app = builder.Build();
 
