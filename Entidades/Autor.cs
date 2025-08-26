41a830f01cfd719d5e73bb16159de32c7676e02f
odelgadillo <odelgadillo@outlook.com>
2025-08-26 09:34:56 -0400

    Agregar atributo de validacion a la clase autor

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -7,6 +7,7 @@ public class Autor
     public int Id { get; set; }
 
     [Required]
+    [StringLength(150, ErrorMessage ="El campo {0} debe tener {1} caracteres o menos")]
     public required string Nombre { get; set; }
 
     public List<Libro> Libros { get; set; } = new();
