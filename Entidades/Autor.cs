0c4cd3bd060ac2c2b73fb1537cb902c3bd6a51df
odelgadillo <odelgadillo@outlook.com>
2025-08-27 11:23:49 -0400

    Nueva migracion y de columnas en autores

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -7,10 +7,18 @@ public class Autor
 {
     public int Id { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "El campo {0} es requerido")]
     [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
     [PrimeraLetraMayuscula]
-    public required string Nombre { get; set; }
+    public required string Nombres { get; set; }
+
+    [Required(ErrorMessage = "El campo {0} es requerido")]
+    [StringLength(150, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
+    [PrimeraLetraMayuscula]
+    public required string Apellidos { get; set; }
+
+    [StringLength(20, ErrorMessage = "El campo {0} debe tener {1} caracteres o menos")]
+    public string? Identificacion { get; set; }
 
     public List<Libro> Libros { get; set; } = new();
 }
