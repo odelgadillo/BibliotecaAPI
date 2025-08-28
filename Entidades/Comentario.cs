f41e73f802ac23ffa5b0ab97479ee18ae2dec601
odelgadillo <odelgadillo@outlook.com>
2025-08-28 14:06:50 -0400

    Hacer requerido el cuerpo del comentario

--- Entidades/Comentario.cs
+++ Entidades/Comentario.cs
@@ -1,10 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BibliotecaAPI.Entidades;
 
 public class Comentario
 {
     public Guid Id { get; set; }
+    [Required]
     public required string Cuerpo { get; set; }
     public DateTime FechaPublicacion { get; set; }
     public int LibroId { get; set; }
