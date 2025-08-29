93324559b77e708b1fc0f9f65a302e752795c92c
odelgadillo <odelgadillo@outlook.com>
2025-08-29 17:39:31 -0400

    agregar endpoint para ObtenerAutoresPorIds

--- Controllers/AutoresColeccionController.cs
+++ Controllers/AutoresColeccionController.cs
@@ -3,6 +3,7 @@ using BibliotecaAPI.Datos;
 using BibliotecaAPI.DTOs;
 using BibliotecaAPI.Entidades;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BibliotecaAPI.Controllers
 {
@@ -18,6 +19,39 @@ namespace BibliotecaAPI.Controllers
             this.context = context;
             this.mapper = mapper;
         }
+
+        [HttpGet("{ids}", Name = "ObtenerAutoresPorIds")] //api/autores-coleccion/1,2,3
+        public async Task<ActionResult<List<AutorConLibrosDTO>>> Get(string ids)
+        {
+            var idsColeccion = new List<int>();
+            foreach (var id in ids.Split(","))
+            {
+                if (int.TryParse(id, out int idInt))
+                {
+                    idsColeccion.Add(idInt);
+                }
+            }
+
+            if (!idsColeccion.Any())
+            {
+                ModelState.AddModelError(nameof(ids), "Ningun Id fue encontrado");
+                return ValidationProblem();
+            }
+
+            var autores = await context.Autores
+                                .Include(x => x.Libros)
+                                    .ThenInclude(x => x.Libro)
+                                .Where(x => idsColeccion.Contains(x.Id))
+                                .ToListAsync();
+            if (autores.Count != idsColeccion.Count)
+            {
+                return NotFound();
+            }
+
+            var autoresDTO = mapper.Map<List<AutorConLibrosDTO>>(autores);
+            return autoresDTO;
+        }
+
 
         [HttpPost]
         public async Task<ActionResult> Post(IEnumerable<AutorCreacionDTO> autoresCreacionDTO)
