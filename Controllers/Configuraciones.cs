f85e962ae779c2e69c8728064817f2fcd298af42
odelgadillo <odelgadillo@outlook.com>
2025-09-02 10:24:16 -0400

    Ejemplo de GetChilden del IConfiguration

--- Controllers/Configuraciones.cs
+++ Controllers/Configuraciones.cs
@@ -8,11 +8,41 @@ namespace BibliotecaAPI.Controllers
     public class Configuraciones : ControllerBase
     {
         private readonly IConfiguration configuration;
+        private readonly IConfigurationSection seccion_01;
+        private readonly IConfigurationSection seccion_02;
 
         public Configuraciones(IConfiguration configuration)
         {
             this.configuration = configuration;
+            seccion_01 = configuration.GetSection("seccion1");
+            seccion_02 = configuration.GetSection("seccion2");
         }
+
+        [HttpGet("seccion_01")]
+        public ActionResult GetSeccion01()
+        {
+            var nombre = seccion_01.GetValue<string>("nombre");
+            var edad = seccion_01.GetValue<int>("edad");
+
+            return Ok(new { nombre, edad });
+        }
+
+        [HttpGet("seccion_02")]
+        public ActionResult GetSeccion02()
+        {
+            var nombre = seccion_02.GetValue<string>("nombre");
+            var edad = seccion_02.GetValue<int>("edad");
+
+            return Ok(new { nombre, edad });
+        }
+
+        [HttpGet("obtenertodos")]
+        public ActionResult GetObtenerTodos()
+        {
+            var hijos = configuration.GetChildren().Select(x => $"{x.Key}: {x.Value}");
+            return Ok(new { hijos });
+        }
+
 
         [HttpGet]
         public ActionResult<string> Get()
